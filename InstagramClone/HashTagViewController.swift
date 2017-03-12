0c605e29848a97b559f00ce1bbea7ba4173bbdb4
Zero2Launch <zero2launch@Dungs-MacBook-Pro.local>
2017-03-11 23:52:06 -0500

    first commit

---
+++ InstagramClone/HashTagViewController.swift
@@ -0,0 +1,35 @@
+//
+//  HashTagViewController.swift
+//  InstagramClone
+//
+//  Created by The Zero2Launch Team on 3/11/17.
+//  Copyright © 2017 The Zero2Launch Team. All rights reserved.
+//
+
+import UIKit
+
+class HashTagViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ InstagramClone/NotificationApi.swift
@@ -0,0 +1,9 @@
+//
+//  NotificationApi.swift
+//  InstagramClone
+//
+//  Created by The Zero2Launch Team on 3/11/17.
+//  Copyright © 2017 The Zero2Launch Team. All rights reserved.
+//
+
+import Foundation
